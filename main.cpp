b106b3ef0a8df24a8c4e8e79d59bc49b8a666447
Aiden <marcin.madry@interia.com>
2022-04-19 18:40:06 +0200

    Small code cleanup in preparation for newer versions of windio

--- main.cpp
+++ main.cpp
@@ -7,23 +7,22 @@
 #include <algorithm>
 
 // Windows specific
-#define WIN32_LEAN_AND_MEAN
 #define WINDIO_IMPLEMENTATION
-
 #include "./windio/windio.hpp"
+
 #include <Windows.h>
 #include <mmsystem.h>
 #include <conio.h>
 
 // For compliers that implement it
 #pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "user32.lib")
 
-// TODO(Aiden): When no MIDI device is detected use keyboard.
-
-static const std::string note_letters[12] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
 static constexpr double FREQ_RATIO = 1.05946309436;
-static output_settings settings;
-static std::vector<double> frequencies;
+
+static const char* note_letters[12] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+static windio_settings settings;
+static std::vector<float> frequencies;
 
 enum MIDIEvent : uint8_t {
     NOTE_OFF = 0x80,
@@ -45,48 +44,27 @@ enum MIDINoteName {
     NOTE_B,
 };
 
-struct MIDINote {
+struct MIDINote
+{
     MIDINoteName note;
     int octave;
-    double frequency;
+    float frequency;
 };
-
-void no_error(const MMRESULT& result)
-{
-    // TODO(Aiden): Properly handle ERROR(s).
-    
-    if (result != MMSYSERR_NOERROR) {
-      fprintf(stderr, "Error calling one of the MIDI functions!");
-      exit(1);
-    }
-} 
-
-void cleanup(const HMIDIIN& handle)
-{
-    if (handle) {
-      midiInStop(handle);
-      midiInClose(handle);
-    }
-}
 
 MIDINote get_note_from_number(uint8_t note_number)
 {
-    MIDINoteName note = static_cast<MIDINoteName>(note_number % 12);
-    int octave = (note_number / 12) - 1;
-
-    int half_steps_count = note + 12 * octave - 57;
-    double frequency = 440 * pow(FREQ_RATIO, half_steps_count);
+    MIDINote midi_note = {};
     
-    MIDINote midi_note = {
-      note,
-      octave,
-      frequency
-    };
+    midi_note.note = static_cast<MIDINoteName>(note_number % 12);
+    midi_note.octave = (note_number / 12) - 1;
+
+    // NOTE(Aiden): we subtract 57 because it is A4 --> 440Hz
+    int half_steps_count = (12 * midi_note.octave) + midi_note.note - 57;
+    midi_note.frequency = 440 * pow(FREQ_RATIO, half_steps_count);
     
     return midi_note;
 }
 
-// TODO(Aiden): Clear the sound, no more pops, make sure chords sound good.
 void CALLBACK MidiInProc(HMIDIIN hMidiIn, UINT wMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2)
 {
     // Not interesed currently
@@ -100,65 +78,72 @@ void CALLBACK MidiInProc(HMIDIIN hMidiIn, UINT wMsg, DWORD_PTR dwInstance, DWORD
       uint8_t velocity = (dwParam1 >> 8 * 2) & 0xff;
 	      
       switch (msg_indication) {
-	  case NOTE_ON: {	      
-	      MIDINote midi_note = get_note_from_number(note_number);
-
-	      frequencies.push_back(midi_note.frequency);
-	      settings.windioPlayMultiple(frequencies);
+          case NOTE_ON: {	      
+              MIDINote midi_note = get_note_from_number(note_number);
+              frequencies.push_back(midi_note.frequency);
 	      
-	      printf("================\n");
-	      printf("Note: %s Octave: %d Frequency: %.2f, Velocity: %d\n",
-		     note_letters[midi_note.note].c_str(),
-		     midi_note.octave,
-		     midi_note.frequency,
-		     velocity);
-	  } break;
+              printf("================\n");
+              printf("Note: %s Octave: %d Frequency: %.2f, Velocity: %d\n",
+                     note_letters[midi_note.note],
+                     midi_note.octave,
+                     midi_note.frequency,
+                     velocity);
+          } break;
 
-	  case NOTE_OFF: {
-	      MIDINote midi_note = get_note_from_number(note_number);
-	      frequencies.erase(std::remove(frequencies.begin(), frequencies.end(), midi_note.frequency), frequencies.end());
-	      
-	      settings.windioPlayMultiple(frequencies);
-	  } break;
+          case NOTE_OFF: {
+              MIDINote midi_note = get_note_from_number(note_number);
+              frequencies.erase(std::remove(frequencies.begin(), frequencies.end(), midi_note.frequency), frequencies.end());	      
+          } break;
       }
+
+      windioPlayMultiple(settings, frequencies, WAVE_SIN);
     }
 }
 
 int main(int argc, char* argv[])
 {
-    // TODO(Aiden): Used later
+    // NOTE(Aiden): Used later
     (void) argc;
     (void) argv;
-    
+
     UINT devices = midiInGetNumDevs();
     MIDIINCAPS caps = {};
-    HMIDIIN handle_in;
+    HMIDIIN handle;
+    MMRESULT device_result = midiInGetDevCaps(0, &caps, sizeof(caps));
 
-    no_error(midiInGetDevCaps(0, &caps, sizeof(caps)));
-
+    // TODO: When no MIDI device is detected use keyboard.
+    assert((device_result == MMSYSERR_NOERROR) && "[ERROR]: No MIDI device found\n");
+    windioInitializeSettings(settings);
+    
     printf("Input MIDI devices found: %d\n", devices);
     printf("Device currently used: %s\n", caps.szPname);
-    
-    no_error(midiInOpen(&handle_in, 0, reinterpret_cast<DWORD_PTR>(MidiInProc), 0, CALLBACK_FUNCTION));
+
+    MMRESULT open_result = midiInOpen(&handle, 0, reinterpret_cast<DWORD_PTR>(MidiInProc), 0, CALLBACK_FUNCTION);
+    assert((open_result == MMSYSERR_NOERROR) && "[ERROR]: MIDI device could not be opened\n");
+
     {
-      bool is_playing = true;
-      char c;
+        bool is_playing = true;
+        char c;
 
-      no_error(midiInStart(handle_in));
-      printf("Started receiving input, press [Q] to exit...\n");
+        MMRESULT start_result = midiInStart(handle);
+        assert((start_result == MMSYSERR_NOERROR) && "[ERROR]: Could not properly start MIDI procedure\n");
+        
+        printf("Started receiving input, press [Q] to exit...\n");
 
-      while (is_playing) {
-          c = _getch();
+        while (is_playing) {
+            c = _getch();
 
-          switch (c) {
-            case 113:
-            case 81:
-                is_playing = false;
-                break;
-          }
-      }
+            switch (c) {
+                case 113:
+                case 81:
+                    is_playing = false;
+                    break;
+            }
+        }
     }
 
-    cleanup(handle_in);
+    midiInStop(handle);
+    midiInClose(handle);
+    
     return 0;
 }
